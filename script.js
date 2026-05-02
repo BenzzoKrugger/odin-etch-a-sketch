9283d18ec55672be9951ec3bb73e16352794530d
Benzzo Krugger <benzzo.krugger@proton.me>
2026-05-02 23:20:27 +0200

    add reset grid function

--- script.js
+++ script.js
@@ -1,32 +1,44 @@
 const container = document.querySelector("#grid-container");
 
-function createGrid(num) {
+function generateGrid(num) {
   for (let i = 0; i < num; i++) {
     for (let j = 0; j < num; j++) {
       let element = document.createElement("div");
       let elementSize = 800 / num;
 
-      element.classList.add("element", "flex-wrap", `size-[${elementSize}px]`);
+      element.classList.add("element", "flex-wrap");
+      element.style.width = elementSize + "px";
+      element.style.height = elementSize + "px";
 
       container.appendChild(element);
     }
   }
+
+  const els = document.querySelectorAll(".element");
+
+  for (const el of els) {
+    el.addEventListener("mouseover", (e) => {
+      e.target.classList.add("bg-blue-500");
+    });
+  }
 }
+
+function clearGrid() {
+  container.innerHTML = "";
+}
+
 // Create Grid
-createGrid(16);
+generateGrid(16);
 
+const resetBtn = document.querySelector("#reset");
+resetBtn.addEventListener("click", resetGrid);
 
-// Handle hover
-function handleHover(e){
-    e.target.classList.add("bg-blue-500")
-
+function resetGrid(e) {
+  gridSize = parseInt(prompt("How big grid do you want?"));
+  if (gridSize < 1 || gridSize > 100) {
+    alert("Grid should be in interval 1-100");
+  } else {
+    clearGrid();
+    generateGrid(gridSize);
+  }
 }
-
-const els = document.querySelectorAll(".element");
-console.log(els);
-
-for (const el of els) {
-  el.addEventListener("mouseover", handleHover);
-  
-}
-
